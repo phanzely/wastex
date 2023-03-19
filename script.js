565beb024985f26b70e921504ae85f486ad4f0cf
Preston Hanzely <103866223+phanzely@users.noreply.github.com>
2023-03-18 21:17:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ const fruitVeggiesLink = document.querySelector("#fruit-veggies-link");
 const otherLink = document.querySelector("#other-link");
 const dataTable = document.querySelector("#data-table");
 
+
+
+function showBuyPopup(sku, quantity, seller, price) {
+  var popup = document.getElementById("buy-popup");
+  popup.innerHTML = "Successfully bought 1.5 mega tons at price $312.41 from Cayuga Medical Ingredients with SKU N16a727n82Sdf2t";
+  popup.style.display = "block";
+}
+
+
+
+
+
+
+
 dairyLink.addEventListener("click", function () {
   loadData("dairy");
 });
@@ -29,29 +43,3 @@ const tableBody = document.querySelector('table tbody');
 
 const newRow = document.createElement('tr');
 
-const skuCell = document.createElement('td');
-skuCell.textContent = 'N16a727n82Sdf2t';
-newRow.appendChild(skuCell);
-
-const quantityCell = document.createElement('td');
-quantityCell.textContent = '1.5 metric tons';
-newRow.appendChild(quantityCell);
-
-const sellerCell = document.createElement('td');
-sellerCell.textContent = 'Cayuga Medical Ingredients';
-newRow.appendChild(sellerCell);
-
-const priceCell = document.createElement('td');
-priceCell.textContent = '$312.41';
-newRow.appendChild(priceCell);
-
-const buyCell = document.createElement('td');
-const buyButton = document.createElement('button');
-buyButton.textContent = 'BUY';
-buyButton.addEventListener('click', () => {
-  showBuyPopup(quantityCell.textContent, priceCell.textContent.slice(1), sellerCell.textContent, skuCell.textContent);
-});
-buyCell.appendChild(buyButton);
-newRow.appendChild(buyCell);
-
-tableBody.appendChild(newRow);
