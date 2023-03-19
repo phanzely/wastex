836b92daad055c3e730d8cfa3579a0dec9c5c429
Preston Hanzely <103866223+phanzely@users.noreply.github.com>
2023-03-18 21:28:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-const dairyLink = document.querySelector("https://phanzely.github.io/wastex/dairy.html");
+const dairyLink = document.querySelector("#dairy-link");
 const meatLink = document.querySelector("#meat-link");
 const fruitVeggiesLink = document.querySelector("#fruit-veggies-link");
 const otherLink = document.querySelector("#other-link");
 const dataTable = document.querySelector("#data-table");
-
-
-
-function showBuyPopup(sku, quantity, seller, price) {
-  var popup = document.getElementById("buy-popup");
-  popup.innerHTML = "Successfully bought 1.5 mega tons at price $312.41 from Cayuga Medical Ingredients with SKU N16a727n82Sdf2t";
-  popup.style.display = "block";
-}
-
-
-
-
-
 
 
 dairyLink.addEventListener("click", function () {
@@ -34,12 +21,14 @@ otherLink.addEventListener("click", function () {
   loadData("other");
 });
 
-function showBuyPopup(quantity, price, seller, sku) {
-  const message = `Successfully bought ${quantity} at $${price} from ${seller} with SKU ${sku}.`;
-  alert(message);
-}
 
 const tableBody = document.querySelector('table tbody');
 
 const newRow = document.createElement('tr');
 
+
+function showBuyPopup(sku, quantity, seller, price) {
+  var popup = document.getElementById("buy-popup");
+  popup.innerHTML = "Successfully bought 1.5 mega tons at price $312.41 from Cayuga Medical Ingredients with SKU N16a727n82Sdf2t";
+  popup.style.display = "block";
+}
